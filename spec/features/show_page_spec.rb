fbbfc024afc44c142bef603d6148a9088898d9c3
Jamie Little <jamie@jamielittle.org>
2018-04-09 16:52:01 -0500

    Remove link from show page back to Iris
    This commit removes a link that appears in the
    show page that links you back to the show page in
    Iris.
    
    Closes curationexperts/alexandria#89

--- spec/features/show_page_spec.rb
+++ spec/features/show_page_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe 'viewing content on the aster show page', type: :feature do
     visit solr_document_path 'gford-20140000-010015_belvegr'
     expect(page).to have_content 'Vegetation, Maya Forest, Belize, 1995'
   end
+  scenario 'viewing the show page and not seeing links back to Geoworks/Hyrax/Iris' do
+    visit solr_document_path 'gford-20140000-010015_belvegr'
+    expect(page).not_to have_link 'http://iris-demo.curationexperts.com/concern/vector_works/7d278t00k'
+  end
 end
